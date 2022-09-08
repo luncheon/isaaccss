17194c16d5866f02dc07b5e24b093b0d1d62622a
luncheon <takeuck+github@gmail.com>
2022-09-08 11:11:38 +0900

    remove parseHtml

--- pkg/api/parseHtml.d.ts
+++
@@ -1,2 +0,0 @@
-import type { ParserOptions, Style } from "./types.js";
-export declare const parseHtml: (content: string, options?: ParserOptions, collectTo?: Map<string, Style>) => Map<string, Style>;
